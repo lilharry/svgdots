15c482d58a16b7e425c7114d97762c7ce01b561e
Harry Lum <hlum1@stuy.edu>
2017-03-01 10:05:32 -0500

    fin

--- svg.js
+++ svg.js
@@ -1,18 +1,42 @@
 
 var svgc = document.getElementById("svgcontainer");
-svgc.setAttribute("width",500);
-svgc.setAttribute("height",500);
 
+var firstPoint = true;
+var lastX = 0;
+var lastY = 0;
 
-var click = function(e){
-    c = document.createElementNS(
-	"http://www.w3.org/2000/svg",
-	"circle");
-    c.setAtrribute("cx",e.clientX);
-    c.setAttribute("cy",e.clientY);
-    c.setAttribute("r",10);
-    svgc.appendChild(c)
-}
+function makeCircle(x, y, r){
+
+    //circle
+    var c = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "circle");
+    c.setAttribute("cx",x);
+    c.setAttribute("cy",y);
+    c.setAttribute("r",r);
+
+    c.setAttribute("stroke-width","1");
+    c.setAttribute("fill","red");
+    svgc.appendChild(c);
+
+    //line
+    if (firstPoint){
+        firstPoint = false;
+    }else{
+        var line = document.createElementNS("http://www.w3.org/2000/svg","line");
+        line.setAttribute("x1",lastX);
+        line.setAttribute("y1",lastY);
+        line.setAttribute("x2",x);
+        line.setAttribute("y2",y);
+        line.setAttribute("style","stroke:rgb(0,0,0);stroke-width:1");
+        svgc.appendChild(line);
+    }
+    lastX = x
+    lastY = y
+
+    
+    
+};
 
 var clear = function(e){
     while (svgc.lastChild) {
@@ -23,3 +47,14 @@ var clear = function(e){
 
 var c = document.getElementById("c")
 c.addEventListener("click",clear)
+
+function getMousePos(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    return [event.clientX - rect.left,event.clientY - rect.top]
+
+};
+
+svgc.addEventListener('click', function(e){
+    var mousePos = getMousePos(svgc, e);
+    makeCircle(mousePos[0], mousePos[1], 10);
+                                        }, false);
